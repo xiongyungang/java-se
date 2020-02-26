c3be2b6ab940a9abfe653e4a17db80331e863d45
xiongyungang <xiongyungang@corp-ci.com>
2020-02-26 22:14:35 +0800

    22 14

--- src/com/collectionDemo/HashSetDemo.java
+++ src/com/collectionDemo/HashSetDemo.java
@@ -1,6 +1,7 @@
 package com.collectionDemo;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * HashSet的基本操作
@@ -13,12 +14,25 @@ public class HashSetDemo {
 	public static void main(String[] args) {
 		HashSet<String> hashSet = new HashSet<String>();
 
+		hashSet.add(null);
+		System.out.println("hashSet = " + hashSet.isEmpty()); // false
+
 		hashSet.add("hello");
-		hashSet.add("hello");
-		hashSet.add("hello");
-		hashSet.add("hello");
-//		String s = null;
-//		s.hashCode();
+		hashSet.add("world");
+		hashSet.add("!");
+
+		//  支持迭代器和加强for遍历
+		for (Object e :
+				hashSet) {
+			System.out.println("e = " + e);
+		}
+
+		Iterator<String> iterator = hashSet.iterator();
+		while (iterator.hasNext())
+		{
+			String next = iterator.next();
+			System.out.println("next = " + next);
+		}
 
 		System.out.println(hashSet);
 	}
--- src/com/collectionDemo/TreeSetDemo1.java
+++ src/com/collectionDemo/TreeSetDemo1.java
@@ -1,5 +1,8 @@
 package com.collectionDemo;
 
+import com.Employee;
+import com.Person;
+
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.TreeSet;
@@ -15,11 +18,20 @@ public class TreeSetDemo1 {
 	public static void main(String[] args) {
 		//使用无参构造，默认自然排序
 		//TreeSet<Integer> treeSet = new TreeSet<Integer>();
-		
-		//使用比较器对象
-		//TreeSet<Integer> treeSet = new TreeSet<Integer>(new MyComparator());
-		
-		//使用比较器对象，匿名内部内
+
+		TreeSet<TestNoCompa> employees = new TreeSet<>();
+		//employees.add(new TestNoCompa());  //java.lang.ClassCastException
+
+		//比较器排序，构造传入比较器对象
+		TreeSet<Integer> treeSetDemo = new TreeSet<Integer>(new MyComparator());
+		treeSetDemo.add(3);
+		treeSetDemo.add(3);
+		treeSetDemo.add(4);
+		treeSetDemo.add(2);
+		treeSetDemo.add(1);
+		System.out.println("treeSetDemo = " + treeSetDemo); // treeSetDemo = [1, 2, 3, 4]
+
+		//比较器排序，使用比较器对象，匿名内部内方式
 		TreeSet<Integer> treeSet = new TreeSet<Integer>(new Comparator<Integer>() {
 
 			@Override
@@ -27,16 +39,13 @@ public class TreeSetDemo1 {
 				return o2-o1;
 			}
 		});
-		
-		//自然排序+去重
 		treeSet.add(5);
 		treeSet.add(2);
 		treeSet.add(3);
 		treeSet.add(4);
 		treeSet.add(1);
 		treeSet.add(5);
-		
-		System.out.println(treeSet);
+		System.out.println("treeSet = " + treeSet);  //  treeSet = [5, 4, 3, 2, 1]
 	}
 }
 
@@ -44,6 +53,11 @@ class MyComparator implements Comparator<Integer>{
 
 	@Override
 	public int compare(Integer o1, Integer o2) {
-		return o2-o1;
+		return o1 - o2;
 	}
+}
+
+class TestNoCompa
+{
+	int ele;
 }
